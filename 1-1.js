90e3711f3188b7f4775eb136157159b5bade583e
AlphaKretin <deewhydeeecks@gmail.com>
2019-12-01 15:38:43 +1030

    Day 1

---
+++ 1-1.js
@@ -0,0 +1,13 @@
+const fs = require("fs").promises;
+fs.readFile("inputs/1.txt", "utf-8").then(input => {
+    const nums = input.split("\n").map(n => parseInt(n, 10));
+    let sum = 0;
+    for (const num of nums) {
+        const div = num/3;
+        console.log(div);
+        const round = Math.floor(div);
+        const final = round - 2;
+        sum += final;
+    }
+    console.log(sum);
+})
---
+++ 1-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs").promises;
+fs.readFile("inputs/1.txt", "utf-8").then(input => {
+    const nums = input.split("\n").map(n => parseInt(n, 10));
+    let sum = 0;
+    for (const num of nums) {
+        const div = num/3;
+        const round = Math.floor(div);
+        const final = round - 2;
+        if (final > 0) {
+            sum += final;
+            nums.push(final);
+        }
+    }
+    console.log(sum);
+})
